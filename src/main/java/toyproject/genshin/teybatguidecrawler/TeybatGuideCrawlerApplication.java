bc3cb859fd6fbbc3025e8e04eb81c0a7a8647fbf
ahma0 <dsd932@naver.com>
2024-06-06 18:30:24 +0900

    chore: create project

---
+++ src/main/java/toyproject/genshin/teybatguidecrawler/TeybatGuideCrawlerApplication.java
@@ -0,0 +1,13 @@
+package toyproject.genshin.teybatguidecrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeybatGuideCrawlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeybatGuideCrawlerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/toyproject/genshin/teybatguidecrawler/TeybatGuideCrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package toyproject.genshin.teybatguidecrawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeybatGuideCrawlerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
