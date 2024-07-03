4e6625e661e3024c342959abc66f50123e9378a6
ahma0 <dsd932@naver.com>
2024-07-04 00:41:50 +0900

    feat: PropertyNotFoundException 구현

---
+++ src/main/java/toyproject/genshin/teybatguidecrawler/exception/PropertyNotFoundException.java
@@ -0,0 +1,9 @@
+package toyproject.genshin.teybatguidecrawler.exception;
+
+public class PropertyNotFoundException extends RuntimeException {
+
+    public PropertyNotFoundException(String message) {
+        super(message);
+    }
+
+}
