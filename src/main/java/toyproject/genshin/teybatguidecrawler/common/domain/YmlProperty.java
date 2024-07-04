b8cd847faa6066582aa74256fede936089c831c8
ahma0 <dsd932@naver.com>
2024-07-04 22:40:07 +0900

    feat: YmlProperty 기능 추가

--- src/main/java/toyproject/genshin/teybatguidecrawler/common/domain/YmlProperty.java
+++ src/main/java/toyproject/genshin/teybatguidecrawler/common/domain/YmlProperty.java
@@ -1,9 +1,13 @@
 package toyproject.genshin.teybatguidecrawler.common.domain;
 
+import jakarta.el.PropertyNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.bind.Binder;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 import toyproject.genshin.teybatguidecrawler.common.domain.value.SiteProperties;
+
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -13,6 +17,11 @@ public class YmlProperty {
 
     public String getProperty(SiteProperties name) {
         return environment.getProperty(name.getPropertyLocation());
+    }
+
+    public List<String> getProperties(SiteProperties name) {
+        Binder binder = Binder.get(environment);
+        return binder.bind(name.getPropertyLocation(), List.class).orElseThrow(PropertyNotFoundException::new);
     }
 
 }
