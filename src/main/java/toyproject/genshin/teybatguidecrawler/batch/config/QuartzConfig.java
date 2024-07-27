14c0ffb53e9e9aa0739e350a248c5513f2db1d62
ahma0 <dsd932@naver.com>
2024-07-28 02:48:00 +0900

    feat: JobConfig, QuartzConfig 수정

--- src/main/java/toyproject/genshin/teybatguidecrawler/batch/config/QuartzConfig.java
+++ src/main/java/toyproject/genshin/teybatguidecrawler/batch/config/QuartzConfig.java
@@ -1,10 +1,12 @@
 package toyproject.genshin.teybatguidecrawler.batch.config;
 
-import org.quartz.Scheduler;
-import org.quartz.SchedulerException;
+import org.quartz.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
+import toyproject.genshin.teybatguidecrawler.batch.launcher.BatchJobLauncher;
+
+import java.util.List;
 
 @Configuration
 public class QuartzConfig {
@@ -15,8 +17,24 @@ public class QuartzConfig {
     }
 
     @Bean
-    public Scheduler scheduler(SchedulerFactoryBean schedulerFactoryBean) throws SchedulerException {
+    public Scheduler scheduler(
+            SchedulerFactoryBean schedulerFactoryBean, List<JobDetail> jobDetails, List<Trigger> triggers
+    ) throws SchedulerException {
+
         Scheduler scheduler = schedulerFactoryBean.getScheduler();
+
+        for (JobDetail jobDetail : jobDetails) {
+            if (!scheduler.checkExists(jobDetail.getKey())) {
+                scheduler.addJob(jobDetail, true);
+            }
+        }
+
+        for (Trigger trigger : triggers) {
+            if (!scheduler.checkExists(trigger.getKey())) {
+                scheduler.scheduleJob(trigger);
+            }
+        }
+
         scheduler.start();
         return scheduler;
     }
